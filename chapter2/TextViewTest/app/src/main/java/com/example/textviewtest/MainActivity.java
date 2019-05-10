b06ffed3b45620b9efc06f77cbb34c475cfc46af
jfq <jfq3301@163.com>
2019-05-11 01:37:31 +0800

    create chapter2

---
+++ chapter2/TextViewTest/app/src/main/java/com/example/textviewtest/MainActivity.java
@@ -0,0 +1,44 @@
+/**
+ * @brief textview/ editText test
+ * @author jfq3301
+ * @date 2019.5.8
+ */
+
+package com.example.textviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    RadioGroup rg;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.edittext_test);
+//        setContentView(R.layout.button_test);
+//        setContentView(R.layout.check_button_test);
+//        rg = findViewById(R.id.rg);
+//        tv = findViewById(R.id.show);
+//        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(RadioGroup group, int checkedId) {
+//                String tips;
+//
+//                if (checkedId == R.id.female){
+//                    tips = "It's female.";
+//                } else {
+//                    tips = "It's male.";
+//                }
+//                tv.setText(tips);
+//            }
+//        });
+
+    }
+}
