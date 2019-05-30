3bf5ac139ab5aa1e0edd4f4490898dcebc33ae93
jfq <jfq3301@163.com>
2019-05-31 06:48:35 +0800

    grid view test.

--- chapter2/TextViewTest/app/src/main/java/com/example/textviewtest/MainActivity.java
+++ chapter2/TextViewTest/app/src/main/java/com/example/textviewtest/MainActivity.java
@@ -6,51 +6,66 @@
 
 package com.example.textviewtest;
 
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.os.SystemClock;
+//import android.graphics.Bitmap;
+//import android.graphics.drawable.BitmapDrawable;
+//import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+//import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.Chronometer;
-import android.widget.CompoundButton;
+import android.widget.AdapterView;
+//import android.widget.ArrayAdapter;
+//import android.widget.AutoCompleteTextView;
+//import android.widget.Button;
+//import android.widget.CheckBox;
+//import android.widget.Chronometer;
+//import android.widget.CompoundButton;
+import android.widget.GridView;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.MultiAutoCompleteTextView;
-import android.widget.QuickContactBadge;
-import android.widget.RadioGroup;
-import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.ToggleButton;
+//import android.widget.LinearLayout;
+//import android.widget.MultiAutoCompleteTextView;
+//import android.widget.QuickContactBadge;
+//import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
+//import android.widget.Switch;
+//import android.widget.TextView;
+//import android.widget.ToggleButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
 //    RadioGroup rg;
 //    TextView tv;
-    Chronometer ch;
-    Button start;
-
-    ToggleButton toggle;
-    Switch switcher;
-
-    ImageView image1;
-    ImageView image2;
-    int[] images = new int[]{
-            R.drawable.lijiang,
-            R.drawable.qiao,
-            R.drawable.shuangta,
-            R.drawable.shui,
-            R.drawable.xiangbi
+//    Chronometer ch;
+//    Button start;
+//
+//    ToggleButton toggle;
+//    Switch switcher;
+//
+//    ImageView image1;
+//    ImageView image2;
+//    int[] images = new int[]{
+//            R.drawable.lijiang,
+//            R.drawable.qiao,
+//            R.drawable.shuangta,
+//            R.drawable.shui,
+//            R.drawable.xiangbi
+//    };
+//    int currentImg = 2;
+//    private int alpha = 255;
+//
+//    QuickContactBadge badge;
+    GridView grid;
+    ImageView imageView;
+    int[] imageIds = new int[] {
+            R.drawable.bomb5, R.drawable.bomb6,  R.drawable.bomb7,  R.drawable.bomb8,
+            R.drawable.bomb9, R.drawable.bomb10,  R.drawable.bomb11,  R.drawable.bomb12,
+            R.drawable.bomb13, R.drawable.bomb14,  R.drawable.bomb15,  R.drawable.bomb16
     };
-    int currentImg = 2;
-    private int alpha = 255;
-
-    QuickContactBadge badge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -220,23 +235,65 @@ public class MainActivity extends AppCompatActivity {
          * @author fjiang2
          * @date 2019.5.30
          */
-        setContentView(R.layout.auto_complete_text_view_test);
-        AutoCompleteTextView auto = findViewById(R.id.auto);
-        MultiAutoCompleteTextView mauto = findViewById(R.id.mauto);
-        String [] books = new String[]{
-                "crazy java",
-                "crazy ajax",
-                "crazy android",
-                "crazy xml"
-        };
+//        setContentView(R.layout.auto_complete_text_view_test);
+//        AutoCompleteTextView auto = findViewById(R.id.auto);
+//        MultiAutoCompleteTextView mauto = findViewById(R.id.mauto);
+//        String [] books = new String[]{
+//                "crazy java",
+//                "crazy ajax",
+//                "crazy android",
+//                "crazy xml"
+//        };
+//
+//        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_dropdown_item_1line,
+//                books);
+//
+//        auto.setAdapter(aa);
+//        mauto.setAdapter(aa);
+//        mauto.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 
-        ArrayAdapter<String> aa = new ArrayAdapter<String>(this,
-                android.R.layout.simple_dropdown_item_1line,
-                books);
+        /**
+         * @brief grid view test.
+         * @author fjiang2
+         * @date 2019.5.30
+         */
+        setContentView(R.layout.grid_view_test);
+        List<Map<String, Object>> listItems =
+                new ArrayList<Map<String, Object>>();
+        for(int i = 0; i < imageIds.length; i++) {
+            Map<String, Object> listItem = new HashMap<String, Object>();
+            ((HashMap) listItem).put("images", imageIds[i]);
+            ((ArrayList) listItems).add(listItem);
+        }
 
-        auto.setAdapter(aa);
-        mauto.setAdapter(aa);
-        mauto.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+        imageView = findViewById(R.id.imageView);
+        grid = findViewById(R.id.grid);
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,
+                listItems,
+                R.layout.cell,
+                new String[] {"images"},
+                new int[] {R.id.cellImage});
+        grid.setAdapter(simpleAdapter);
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                imageView.setImageResource(imageIds[position]);
+            }
+        });
+
+        grid.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                imageView.setImageResource(imageIds[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
 
     }
 }
