36866ebe3af6801ff1f2029a827c639e2977bffe
jfq <jfq3301@163.com>
2019-06-11 05:00:25 +0800

    calendar datePikcer timePicker

--- chapter2/ToastTest/app/src/main/java/com/example/main/MainActivity.java
+++ chapter2/ToastTest/app/src/main/java/com/example/main/MainActivity.java
@@ -8,35 +8,129 @@ package com.example.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
+//    private int second
+
+    private void showDate(int year, int month, int day,
+                          int hour, int minute) {
+        EditText et = findViewById(R.id.showEditText);
+        et.setText("day: " + year + "." + (month+1) + "." + day + " " + hour + ":" + minute + ":00" );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
-        Button simple = (Button)findViewById(R.id.simple);
-        simple.setOnClickListener(new View.OnClickListener() {
+        /**
+         * @brief datePicker/ timePicker test
+         * @author fjiang2
+         * @date 2019.6.10
+         */
+        setContentView(R.layout.data_time_picker);
+        DatePicker dataPicker = findViewById(R.id.dataPicker);
+        TimePicker timePicker = findViewById(R.id.timePicker);
+
+        Calendar c = Calendar.getInstance();
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+        hour = c.get(Calendar.HOUR);
+        minute = c.get(Calendar.MINUTE);
+
+        dataPicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
             @Override
-            public void onClick(View v) {
-                Toast toast = Toast.makeText(MainActivity.this,
-                        "Simple test tips",
-                        Toast.LENGTH_SHORT);
-                toast.show();
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                MainActivity.this.year = year;
+                MainActivity.this.month = monthOfYear;
+                MainActivity.this.day = dayOfMonth;
+                showDate(year, month, day, hour, minute);
             }
         });
 
-        Button complex = findViewById(R.id.complex);
-        complex.setOnClickListener(new View.OnClickListener() {
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
             @Override
-            public void onClick(View v) {
-
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                MainActivity.this.hour = hourOfDay;
+                MainActivity.this.minute = minute;
+                showDate(year, month, day, hour, minute);
             }
         });
+
+
+
+
+        /**
+         * @brief calendar test
+         * @author fjiang2
+         * @date 2019.6.10
+         */
+//        setContentView(R.layout.calendar_view);
+//        CalendarView cv = findViewById(R.id.calendarView);
+//        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+//                Toast.makeText(MainActivity.this,
+//                        "your birthday: " + year +
+//                        " year " + month + " month "
+//                + dayOfMonth + " day", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+//        Button simple = (Button)findViewById(R.id.simple);
+//        simple.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast toast = Toast.makeText(MainActivity.this,
+//                        "Simple test tips",
+//                        Toast.LENGTH_SHORT);
+//                toast.show();
+//            }
+//        });
+//
+//        Button complex = findViewById(R.id.complex);
+//        complex.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast toast = new Toast(MainActivity.this);
+//                toast.setGravity(Gravity.CENTER, 0, 0);
+//
+//                ImageView image = new ImageView(MainActivity.this);
+//                image.setImageResource(R.drawable.ic_launcher_background);
+//
+//                LinearLayout ll = new LinearLayout(MainActivity.this);
+//                ll.addView(image);
+//
+//                TextView tv = new TextView(MainActivity.this);
+//                tv.setText("toast with image");
+//                tv.setTextSize(34);
+//                ll.addView(tv);
+//
+//                toast.setView(ll);
+//                toast.setDuration(Toast.LENGTH_LONG);
+//                toast.show();
+//            }
+//        });
 
     }
 }
